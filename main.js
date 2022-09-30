8b81eecd80bd7ee9db83c76633fcf48bfc6434df
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-09-30 13:08:16 -0400

    added local strage and save buttons for the time slots on calander

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ currentDay.innerText = moment().format("MMM Do YY");
 let blockcontainer = document.getElementById("blocks")
 let times = [9, 10, 11, 12, 1, 2, 3, 4, 5]
 
+let storage = []
+let checkstorage = JSON.parse(localStorage.getItem("time"))
+if (checkstorage === null) localStorage.setItem("time", JSON.stringify([]))
+else storage = checkstorage
 for (let i = 0; i < times.length; i++) {
     let block = document.createElement("div")
-    block.innerHTML = `<span>${times[i]}${times[i] > 0 && times[i] < 6 ? "pm" : "am"}</span>
+    block.innerHTML = `<span>${times[i]}${(times[i] > 0 && times[i] < 6) || times[i] === 12 ? "pm" : "am"}</span>
     <textarea></textarea>
     <button type="button"><i class="far fa-save"></i></button>`
     blockcontainer.append(block)
-}
+    let savebutton = block.querySelector("button")
+    savebutton.addEventListener("click", () => {
+
+    })
+}//If/else statement ternary//*
